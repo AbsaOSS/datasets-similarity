ded101bc1e89e03db68315db0f7ca8d4162db1f3
Olivie Franklova (CZ) <Olivie.Franklova@absa.africa>
2024-04-26 10:22:11 +0200

    Update workflow

--- column2Vec/functions.py
+++ column2Vec/functions.py
@@ -1,3 +1,6 @@
+"""
+Functions usefull for column2Vec.
+"""
 from typing import Any
 
 import numpy as np
@@ -10,6 +13,13 @@ from similarity.Types import NONNUMERICAL
 
 
 def get_data(files: list[str]) -> dict[str, Any]:
+    """
+    Reads all csv files (which name is in files). Creates metadata for them.
+    Save only nonnumerical columns into dictionary. Key is name of column.
+    Value is column.
+    :param files: list names of csv files
+    :return: dictionary of all tables.
+    """
     result = {}
     index = 0
     for i in files:
@@ -27,6 +37,13 @@ def get_data(files: list[str]) -> dict[str, Any]:
 
 
 def get_clusters(vectors_to_cluster: pd.DataFrame, n_clusters: int) -> list[list[str]]:
+    """
+    Creates clusters by KMeans for given vectors.
+
+    :param vectors_to_cluster: embeddings for columns
+    :param n_clusters: number of clusters we want
+    :return: List, for each cluster number it contains list of column names
+    """
     kmeans = KMeans(n_clusters=n_clusters, random_state=0)  # Change n_clusters as needed
     list_of_vectors = np.array(list(vectors_to_cluster.values()))
     kmeans.fit(list_of_vectors)
@@ -43,6 +60,12 @@ def get_clusters(vectors_to_cluster: pd.DataFrame, n_clusters: int) -> list[list
 
 
 def compute_distances(vectors: dict):
+    """
+    Compute distance for each pair of vectors.
+
+    :param vectors: dictionary of embedding vectors
+    :return: matrix with distances
+    """
     res = {}
     for key1, vec1 in vectors.items():
         res[key1] = {}
