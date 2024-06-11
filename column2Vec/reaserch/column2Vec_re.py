5fe536ee221d58297f52e73ac384f655de31db6f
OlivieFranklova <frankoli@fit.cvut.cz>
2024-06-11 11:23:41 +0200

    Update column2Vec_re

--- column2Vec/reaserch/column2Vec_re.py
+++ column2Vec/reaserch/column2Vec_re.py
@@ -9,26 +9,41 @@ from column2Vec.functions import get_nonnumerical_data
 from similarity.Comparator import cosine_sim
 
 FUNCTIONS = [column2vec_as_sentence, column2vec_as_sentence_clean, column2vec_as_sentence_clean_uniq,
-             column2vec_avg, column2vec_weighted_avg, column2vec_sum, column2vec_weighted_sum ]
+             column2vec_avg, column2vec_weighted_avg, column2vec_sum, column2vec_weighted_sum]
 MODEL = 'bert-base-nli-mean-tokens'
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
 # otestovani stability pokazde stejne vysledky
-def stability_test():
-    pass
+def stability_test(column1: pd.Series, function, key):
+    model = SentenceTransformer(MODEL)
+
+    emb1 = function(column1, model, key)
+    emb2 = function(column1, model, key)
+    emb3 = function(column1, model, key)
+    first_second = cosine_sim(emb3, emb2)
+    whole_second = cosine_sim(emb1, emb2)
+    whole_first = cosine_sim(emb3, emb1)
+    all_same = emb1 == emb2 and emb3 == emb1
+    if first_second != 1 or whole_second != 1 or whole_first != 1 or not all_same:
+        print(f"For {key}:\n is not stable\n")
+
 
 # otestovani podobnosti sloupcu se stejnym vektorem
 def similarity_test():
     pass
 
+
 # otestovani ze nestejne sloupce(vektory) jsou dostatecne daleko
-def nonsimilar_test():
+def non_similar_test():
     pass
 
+
 # sloupce ktere maji jen cast dat by meli byt stejne jako sloupce cele
-def partitial_column_test(column1:pd.Series, function, key):
+def partial_column_test(column1: pd.Series, function, key):
     length = column1.size
-    first_half = column1.head(int(length/2))
-    second_half = column1.tail(int(length/2))
+    first_half = column1.head(int(length / 2))
+    second_half = column1.tail(int(length / 2))
     model = SentenceTransformer(MODEL)
 
     emb1 = function(column1, model, key)
@@ -44,6 +59,27 @@ def partitial_column_test(column1:pd.Series, function, key):
               )
 
 
+def test_func(data, test_type):
+    i = 0
+    for name in data.keys():
+        print(f"--- Start Column: {name} ---\n")
+        print("AS SENTENCE")
+        test_type(data[name], column2vec_as_sentence, name)
+        print("AS CLEAR SENTENCE")
+        test_type(data[name], column2vec_as_sentence_clean, name)
+        print("AS CLEAR UNIQUE SENTENCE")
+        test_type(data[name], column2vec_as_sentence_clean_uniq, name)
+        print("AVERAGE")
+        test_type(data[name], column2vec_avg, name)
+        print("WEIGHTED AVERAGE")
+        test_type(data[name], column2vec_weighted_avg, name)
+        print("SUM")
+        test_type(data[name], column2vec_sum, name)
+        print("WEIGHTED SUM")
+        test_type(data[name], column2vec_weighted_sum, name)
+        i += 1
+    # partitial_column_test()
+    # print(f"vypsano: {i}, data {len(data)}")
 
 
 ## todo files from both dir
@@ -57,23 +93,5 @@ fileM2 = os.path.join(THIS_DIR, os.pardir, '../data/netflix_titles.csv')
 
 files = [fileA1, fileA2, fileC1, fileC2, fileC3, fileM1, fileM2]
 data = get_nonnumerical_data(files)
-i = 0
-for name in data.keys():
-    print(f"--- Start Column: {name} ---\n")
-    print("AS SENTENCE")
-    partitial_column_test(data[name], column2vec_as_sentence, name)
-    print("AS CLEAR SENTENCE")
-    partitial_column_test(data[name], column2vec_as_sentence_clean, name)
-    print("AS CLEAR UNIQUE SENTENCE")
-    partitial_column_test(data[name], column2vec_as_sentence_clean_uniq, name)
-    print("AVERAGE")
-    partitial_column_test(data[name], column2vec_avg, name)
-    print("WEIGHTED AVERAGE")
-    partitial_column_test(data[name], column2vec_weighted_avg, name)
-    print("SUM")
-    partitial_column_test(data[name], column2vec_sum, name)
-    print("WEIGHTED SUM")
-    partitial_column_test(data[name], column2vec_weighted_sum, name)
-    i+=1
-# partitial_column_test()
-# print(f"vypsano: {i}, data {len(data)}")
+test_func(data, partial_column_test)
+test_func(data, stability_test)
