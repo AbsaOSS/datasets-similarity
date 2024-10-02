62415749fec130204cad98dd524b0efe3e3ed592
OlivieFranklova <frankoli@fit.cvut.cz>
2024-10-02 12:56:29 +0200

    #25 Add json formater

---
+++ similarityRunner/formators/jason_formater.py
@@ -0,0 +1,10 @@
+import json
+
+from interfaces.OutputFormaterInterface import OutputFormaterInterface
+
+
+class JsonFormater(OutputFormaterInterface):
+
+    def format(self, data: dict) -> json:
+        jsondata = json.dumps(data, indent = 4)
+        return jsondata
